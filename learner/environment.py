15450f2c78bca30deaf8259864aa11b3bc94677b
minamorl <minamorl@users.noreply.github.com>
2017-07-21 22:52:45 +0900

    Take rcp/rmsd as argument of Environment

--- learner/environment.py
+++ learner/environment.py
@@ -8,13 +8,17 @@ class Environment:
                  *,
                  rate_jpy_dollar,
                  owned_capital,
+                 rcp,
+                 rmsd
                  ):
         self.rate_jpy_dollar = rate_jpy_dollar
         self.owned_capital = owned_capital
+        self.rcp = rcp
+        self.rmsd = rmsd
 
     def observe_state(self, time):
         """ Returns environment state at time"""
-        return self.rate_jpy_dollar[time]
+        return (self.rcp[time], self.rmsd[time])
 
     def apply_action(self, time, action):
         """ Returns rewards """
@@ -35,3 +39,7 @@ class Environment:
         reward = math.log(
             self.owned_capital[time + 1] / self.owned_capital[time])
         return reward
+
+    def update_state(self, time):
+        self.rcp[time + 1] = self.rcp[time]
+        self.rmsd[time + 1] = self.rmsd[time]
--- main.py
+++ main.py
@@ -11,30 +11,57 @@ from learner.environment import Environment
 from learner.error import StopLearningIteration
 
 
+def calcrate_rcp_rmsd(dataset, relative_count):
+    rcp_value = np.zeros_like(dataset)
+    rmsd_value = np.zeros_like(dataset)
+    for t in range(dataset.shape[0]):
+        # Calcuration of RCP (relative closing place)
+        mu = 1.0 / relative_count * sum(dataset[t] for _ in range(t - relative_count + 1, t))
+        sigma = 1.0 / relative_count * sum(dataset[t] ** 2 - mu ** 2 for _ in range(t - relative_count+1, t))
+        rcp = (dataset[t] - mu) / (4 * sigma)
+
+        # Calcuration of RMSD (relative moving standard d)
+        mu2 = 1.0 / relative_count * sum(sigma for _ in range(t - relative_count + 1, t))
+        sigma2 = 1.0 / relative_count * sum(sigma ** 2 for _ in range(t - relative_count + 1)) - mu2 ** 2
+        rmsd = (sigma - mu2) / (4 * sigma2)
+
+        rcp_value[t] = rcp
+        rmsd_value[t] = rmsd
+    return rcp_value, rmsd_value
+
 def main():
 
     start = datetime.datetime(2015, 1, 1)
     end = datetime.datetime(2015, 12, 31)
     dataset = web.DataReader("DEXJPUS", "fred", start,  end)['DEXJPUS']
 
-    owned_capital = np.ones((dataset.shape[0], )) * 100
+    owned_capital = np.ones((dataset.shape[0], ))
+    rcp, rmsd = calcrate_rcp_rmsd(dataset, 50)
+
     agent = Agent(step_size=1, discount_factor=0.9, investment_ratio=0.5,
                   q_value=np.zeros((dataset.shape[0], 3)), eps=0.05)
+    
     environment = Environment(
         rate_jpy_dollar=dataset,
-        owned_capital=owned_capital)
+        owned_capital=owned_capital,
+        rcp=rcp,
+        rmsd=rmsd,
+        )
     
     learning_times = 10000
 
     for i in range(learning_times):
         time = random.randrange(dataset.shape[0])
         for j in range(time, dataset.shape[0] - 1):
-            action = agent.choose_action(time)
+            state = environment.observe_state(time)
+            action = agent.choose_action(state[0], state[1])
             reward = environment.apply_action(time, action)
+            next_state = environment.observe_state(time + 1)
             try:
                 agent.update_q_value(time, action, reward)
             except StopLearningIteration as e:
                 break
+            environment.update_state(time)
             time = time + 1
         print(agent.q_value)
 
