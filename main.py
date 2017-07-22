248271b6d87620bfd019b2c8544417da975b5698
minamorl <minamorl@users.noreply.github.com>
2017-07-22 18:47:07 +0900

    Add testing and plot

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from learner.agent import Agent
 from learner.environment import Environment
 from learner.error import StopLearningIteration
 from learner.state import State
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import pandas as pd
 
 
 def calcrate_rcp_rmsd(dataset, relative_count):
@@ -47,6 +50,11 @@ def main():
     end = datetime.datetime(2015, 12, 31)
     dataset = web.DataReader("DEXJPUS", "fred", start,  end)[
         'DEXJPUS'].dropna()
+
+    start2 = datetime.datetime(2016, 1, 1)
+    end2 = datetime.datetime(2016, 12, 31)
+    dataset2 = web.DataReader("DEXJPUS", "fred", start2,  end2)[
+        'DEXJPUS'].dropna()
     owned_capital = np.ones((dataset.shape[0], ))
     rcp, rmsd = calcrate_rcp_rmsd(dataset, 50)
 
@@ -54,16 +62,14 @@ def main():
     rcp_max = rcp.max()
     rmsd_min = rmsd.min()
     rmsd_max = rmsd.max()
-    grid_size = 10
+    grid_size = 1000
     grid_rcp = np.linspace(rcp_min, rcp_max, num=grid_size + 1)
     grid_rmsd = np.linspace(rmsd_min, rmsd_max, num=grid_size + 1)
-    print(grid_rcp)
-    print(grid_rmsd)
 
     def grid_state(state): return State(find_nerest(
         grid_rcp, state.rcp), find_nerest(grid_rmsd, state.rmsd))
 
-    learning_times = 1000
+    learning_times = 10000
 
     agent = Agent(step_size=1, discount_factor=0.9, investment_ratio=0.9,
                   q_value=np.zeros((grid_size * grid_size, 3)), eps=0.05)
@@ -73,10 +79,10 @@ def main():
         rcp=rcp,
         rmsd=rmsd,
     )
+    reward_test = np.zeros((learning_times, ))
 
     for i in range(learning_times):
         time = random.randrange(50, dataset.shape[0])
-        print("{} time iteration".format(i))
         for j in range(time, dataset.shape[0] - 1):
             state = grid_state(environment.observe_state(time))
             action = agent.choose_action(state)
@@ -89,21 +95,31 @@ def main():
             environment.update_state(time)
             time = time + 1
 
-    # testing
-    environment = Environment(
-        rate_jpy_dollar=dataset,
-        owned_capital=owned_capital,
-        rcp=rcp,
-        rmsd=rmsd,
-    )
-    reward = 1
-    for j in range(0, dataset.shape[0] - 2):
-        state = grid_state(environment.observe_state(time))
-        action = agent.choose_action(state)
-        reward *= environment.apply_action(j, action)
-        print("choosed action: {}".format(action))
-        environment.update_state(j)
-    print(reward)
+        # testing
+        environment2 = Environment(
+            rate_jpy_dollar=dataset2,
+            owned_capital=owned_capital,
+            rcp=rcp,
+            rmsd=rmsd,
+        )
+
+        reward2 = 1
+        
+        for j in range(0, dataset2.shape[0] - 2):
+            state = grid_state(environment2.observe_state(time))
+            action = agent.choose_action(state)
+            reward2 *= environment2.apply_action(j, action)
+            environment2.update_state(j)
+        reward_test[i] = reward2
+        
+    fig, ax = plt.subplots()
+    ax.set_xlabel("number of iteration")
+    ax.set_ylabel("Annualized cumulative return[%]")
+    ax.set_title("Annualized cumulative return")
+    plt.plot(pd.expanding_mean(reward_test))
+    plt.grid(True)
+    plt.show()
+
 
 
 main()
